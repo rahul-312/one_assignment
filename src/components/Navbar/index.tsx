df724ea44e81e23f37d168c49617cbbb38637aa9
rahul-312 <rahulsid49@gmail.com>
2025-07-08 23:40:42 +0530

    created the login page

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 const Navbar: React.FC = () => {
   return (
-    <nav className="backdrop-blur-md shadow-md">
-      <div className="w-full  px-4 py-4 flex justify-between items-center">
+    <nav className="backdrop-blur-md shadow-md bg-gray-300 rounded-xl">
+      <div className="w-full  p-4 pr-10 flex justify-between items-center">
         <Link href="/" className="text-xl font-bold text-gray-800">
           CarRental Admin
         </Link>
-        <div className="space-x-4">
-          <Link href="/login" className="text-gray-700 hover:text-blue-600">
+        <div className="">
+          <Link href="/login" className="text-xl text-gray-700 hover:text-blue-600">
             Login
           </Link>
         </div>
